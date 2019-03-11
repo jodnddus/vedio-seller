05656a5a9e6f3f339686e101b00b1a1c048d2cad
jodnddus <lockhost23@gmail.com>
2019-03-12 00:51:18 +0900

    react test code

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+class App extends react.Component {
+    render() {
+        return (
+            <h1>Hi!</h1>
+        )
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import react from "react";
+import reactDom from "react-dom";
+import App from "./components/App";
+
+const rootElement = document.getElementById('root');
+reactDom.render(
+    <App />, rootElement
+);
