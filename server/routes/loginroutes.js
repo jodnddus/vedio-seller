60c8ebdd94ccfd4d8b67d85b280ae31dac95fce2
jodnddus <lockhost23@gmail.com>
2019-03-19 11:47:12 +0900

    get data from mysql

--- server/routes/loginroutes.js
+++ server/routes/loginroutes.js
@@ -1,11 +1,26 @@
-import mysql from 'mysql';
+const mysql = require('mysql');
 
 var connection = mysql.createConnection({
     host    : 'localhost',
     user    : 'root',
     password: '111111',
-    database: 'video_seller'
+    database: 'video_seller',
+    port: '3306'
 })
 
 connection.connect();
 
+exports.register = function(req, res) {
+    connection.query('select * from users', function (error, results, fields) {
+        if(error) {
+            console.log(error);
+        }
+        console.log(results);
+    });
+}
+
+exports.register(); 
+
+connection.end();
+
+//Error: ER_ACCESS_DENIED_ERROR: Access denied for user 'root'@'127.0.0.1' 이슈 해결하기
