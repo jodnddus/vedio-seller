11ce4214c7d23e597d68992fd7f3f0729dfe02dd
jodnddus <lockhost23@gmail.com>
2019-04-05 22:05:32 +0900

    아폴로 부스트로 API 서버에 연결하는 코드

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,7 +1,8 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-    uri: "http://localhost:4000/"
+    uri: "http://localhost:4000/",
+    onError: (e) => { console.log(e) },
 })
 
 export default client;
