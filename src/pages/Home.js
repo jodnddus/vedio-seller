21ce71361746d194680ad95d9c0417f79aad8334
jodnddus <lockhost23@gmail.com>
2019-04-08 19:40:18 +0900

    함수형 컴포넌트로 전환

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,36 +1,33 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import Video from './components/Video';
 import { Query } from 'react-apollo';
 import { GET_MOVIES } from '../queries';
-import './style/Home.css'
+import './style/Home.css';
 
-class Home extends Component {
-    render() {
-        return (
-            <Query query={GET_MOVIES}>{({loading, data, error}) => {
-                if(loading) return "loding"
-                if(error) return "error"
-                return (
-                    <div>
-                        <div id="header">VIDEO-SELLER<span role="img" aria-label="film"> 🎬</span></div>
-                        <div id="container">
-                            {data.videos.map(video => (
-                                <div key={video.id} className="videoItem">
-                                    <Video
-                                        poster={video.medium_cover_image}
-                                        title={video.title}
-                                        id={video.id}
-                                    />
-                                </div>
-                            ))}  
-                        </div>
+export const Home = () => {
+    return (
+        <Query query={GET_MOVIES}>{({ loading, data, error }) => {
+            if (loading) return <h1>LOADING</h1>
+            if (error) return <h1>ERROR</h1>
+            return (
+                <div>
+                    <div id="header">VIDEO-SELLER<span role="img" aria-label="film"> 🎬</span></div>
+                    <div id="container">
+                        {data.videos.map(video => (
+                            <div key={video.id} className="videoItem">
+                                <Video
+                                    poster={video.medium_cover_image}
+                                    title={video.title}
+                                    id={video.id}
+                                />
+                            </div>
+                        ))}
                     </div>
-                );
-            }}
-            </Query>
-        );
-    }
+                </div>
+            );
+        }}
+        </Query>
+    );
 }
 
 export default Home;
