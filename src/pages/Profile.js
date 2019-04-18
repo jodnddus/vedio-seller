a449db5e23700eba719ec1525be3a2a4c94d8010
jodnddus <lockhost23@gmail.com>
2019-04-18 19:26:46 +0900

    프로필 페이지 작성 완료

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import { GET_USER_BY_USERNAME, GET_VIDEO } from '../queryMutation';
+import Video from './components/Video';
 import './style/Profile.css'
 
 class Profile extends Component {
     state = {
-        username: '', 
-        email: '', 
+        username: '',
+        email: '',
         password: '',
     }
 
@@ -23,9 +26,45 @@ class Profile extends Component {
     }
 
     render() {
+        var username = this.state.username;
         return (
-            <div>
-                <h1>{this.state.username}</h1>
+            <div id="profile">
+                <div id="info">
+                    <h1>{this.state.username}'s Profile</h1>
+                    <h1>Info</h1>
+                    {this.state.email}
+                </div>
+                <h1>My Videos</h1>
+                <Query query={GET_USER_BY_USERNAME} variables={{ username }} >
+                    {({ loading, error, data }) => {
+                        if (loading) return `Get your videos`;
+                        if (error) return `Error: ${error}`;
+                        return (
+                            <div id="myvideos">
+                                {data.getUserByUsername.videoId.map(id => (
+                                    //videoId = videoId.toString(),
+                                    //console.log(typeof(videoId)),
+                                    <Query query={GET_VIDEO} variables={{ id: id }}>
+                                        {({ loading, error, data }) => {
+                                            if (loading) return ``;
+                                            if (error) return `${error}`;
+                                            return (
+                                                <div key={data.getVideoById.id} className="myvideoItem">
+                                                    <Video
+                                                        poster={data.getVideoById.medium_cover_image}
+                                                        title={data.getVideoById.title}
+                                                        id={data.getVideoById.id}
+                                                    />
+                                                </div>
+                                                //<h1>{data.getVideoById.title}</h1>
+                                            );
+                                        }}
+                                    </Query>
+                                ))}
+                            </div>
+                        );
+                    }}
+                </Query>
             </div>
         );
     }
